b248360c20fe50dd01d6a7e345366e114677b6b2
Epsorgx <grospemarkgeron@yahoo.com>
2019-01-25 13:44:01 +0800

    Solution #3

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /* ****************************************************************
  * @author: <Mark Gerone F. Grospe>
  * @app name: <Tell Switch case>
- * @app desc: <A program that will accept and integer and execute command based on the input using Switch case statements>
+ * @app desc: <A program that divide the two floating points entered>
  * @history:
  *  - <yyyy/mm/dd> | <name>
  *      -- <description>
@@ -32,31 +32,21 @@ int main() {
     // ************************** TO DO **************************
     // Place your code logic after this comment line
     // ***********************************************************
-    int user_input;
+    float inputFirst;
+    float inputSecond;
 
-    cout << "Hi, there! \nPlease choose a number from 0 to 4 and i will say something: ";
-    cin >> user_input;
+    cout << "This programs performs simple division of two numbers. \n";
 
-    switch (user_input) {
-        case 0 :
-            cout << "\nHello world\n\n";
-            break;
-        case 1 :
-            cout << "\nI am Groot\n\n";
-            break;
-        case 2 :
-            cout << "\nTo the top\n\n";
-            break;
-        case 3 :
-            cout << "\nWhere is the horizon\n\n";
-            break;
-        case 4 :
-            cout << "\nI do not know\n\n";
-            break;
-        default :
-            cout << "\nYeah, I will \n\n";
-            break;
-    }
+    cout << "Please enter the first number: ";
+    cin >> inputFirst;
+    cout << "Please enter the second number: ";
+    cin >> inputSecond;
+
+    float inputQuotient = inputFirst/inputSecond;
+
+    cout << "\nGreat! \n";
+    cout << inputFirst << " divided by " <<  inputSecond << " is equals to " << inputQuotient << "\n\n";
+   
 
     cin.ignore();
     // ********************** DO NOT CHANGE **********************
